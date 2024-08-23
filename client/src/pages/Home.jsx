2473f4e11973533d8e62d76d3793981456106100
Ak4shKr <akashkmr7667@gmail.com>
2024-08-23 17:16:26 +0530

    use token based reload component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,8 @@ import { Col, Row } from "antd";
 import Doctor from "../components/Doctor";
 import { useDispatch } from "react-redux";
 import { showLoading, hideLoading } from "../redux/alertsSlice";
+import { setUser } from "../redux/userSlice";
+import toast from "react-hot-toast";
 function Home() {
   const [doctors, setDoctors] = useState([]);
   const dispatch = useDispatch();
@@ -25,9 +27,39 @@ function Home() {
       dispatch(hideLoading());
     }
   };
+  const token = localStorage.getItem("token");
   useEffect(() => {
-    getData();
-  }, []);
+    const fetchData = async () => {
+      getData();
+      if (token) {
+        dispatch(showLoading());
+        try {
+          const response = await axios.post(
+            "/api/user/get-user-info-by-id",
+            { token: localStorage.getItem("token") },
+            {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+              },
+            }
+          );
+          dispatch(hideLoading());
+          if (response.data.success) {
+            dispatch(setUser(response.data.data));
+          } else {
+            localStorage.clear();
+            toast.info("Please login to continue");
+            console.log(response.data);
+          }
+        } catch (error) {
+          dispatch(hideLoading());
+          console.error("Error fetching user info:", error);
+        }
+      }
+    };
+
+    fetchData();
+  }, [token, dispatch]);
 
   return (
     <Layout>
